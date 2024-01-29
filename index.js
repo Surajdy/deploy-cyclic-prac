70cfa7b6447e442749bf8dac2577f8fffb80f345
suraj yengalwar <suraj.yengalwar111@gmail.com>
2024-01-29 22:13:34 +0530

    app

--- index.js
+++ index.js
@@ -1,21 +1,67 @@
 const express =require('express')
-require("dotenv").config();
-const app =express();
-
-app.get("/welcome",(req,res)=>{
- res.send("Welcome to the world of IT")
+const {connection}=require('./db')
+const {modal}=require('./modal/user.modal')
+require("dotenv").config()
+const jwt =require("jsonwebtoken")
+const bcrypt =require('bcrypt')
+const {authenticateToken}=require('./middleware/authentication')
+const app=express();
+app.use(express.json())
+app.get("/",(req,res)=>{
+  res.send("Welcome to homepage ")
 })
 
 
-app.get("/goodBye",(req,res)=>{
-    res.send("goodBye this is the last lecture of this unit ")
+app.post("/register",async(req,res)=>{
+    const {name,email,password,phone_number,vehicle_type}=req.body;
+    console.log(name,email,password,phone_number,vehicle_type);
+    bcrypt.hash(password, 5, async function(err, hash) {
+        await modal.create({
+            name:name,
+            email:email,
+            password:password,
+            phone_number:phone_number,
+            vehicle_type:vehicle_type
+        });
+        res.send("signup successfull");
+    });
+ 
 })
 
- const PORT =process.env.PORT;
-app.listen(PORT, ()=>{
-    try {
-        console.log("connected to port "+PORT)
-    } catch (error) {
-    console.log("not able to connect to port")        
+app.post("/login",async(req,res)=>{
+    const{name,email,password}=req.body;
+
+    const user =await modal.findOne({name,email});
+
+    const hashed_password=user.password;
+    
+    bcrypt.compare(password, hashed_password, function(err, result) {
+      if(result){
+        const token =jwt.sign({auth:"yes"},"secretcode")
+       console.log(token);
+        return res.send({
+            msg:"login successfull",
+            token:token,
+        })
+      }else {
+        res.send("login failed")
     }
+    });
+})
+
+app.get("/homepage",authenticateToken,async(req,res)=>{
+
+
+    res.send("welcome to homepage")
+    
+})
+
+const PORT =process.env.PORT;
+app.listen(PORT,async()=>{
+ try {
+   await connection;
+    console.log("connected to port "+PORT)
+ } catch (error) {
+    console.log("unable to do connection")
+ }
 })
