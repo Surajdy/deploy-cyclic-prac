d2b0e817ab8eb88ce71ae198514b8e6f80ce1ed0
SurajYengalwar <143103322+Surajdy@users.noreply.github.com>
2024-02-05 19:26:44 +0530

    Delete db.js

--- db.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require("mongoose")
-
-const connection =mongoose.connect('mongodb+srv://surajyengalwar:Suraj%4012345@cluster0.sippxfh.mongodb.net/petrolapp')
-
-module.exports={connection}
